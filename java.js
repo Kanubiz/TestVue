5bc91d0531f5fd2b778ee431f08c75169fc70bb0
kanubiz <duartejhfaria@gmail.com>
2017-07-07 23:00:11 +0100

    Added Loops v-for

--- java.js
+++ java.js
@@ -17,7 +17,21 @@ const app = new Vue({
     available: false,
     nearby: false,
     error: false,
-    success: false
+    success: false,
+    characters: ['Mario', 'Luigi', 'Yoshi', 'Bowser'],
+    ninjas: [{
+        name: 'Ryu',
+        age: 25
+      },
+      {
+        name: 'Yoshi',
+        age: 35
+      },
+      {
+        name: 'Ken',
+        age: 55
+      },
+    ]
   },
   methods: {
     greet: function(time) {
